cd838f778ab55511ca366731cf07cc3c6aaed5ed
S M Faisal <itssmfaisal@gmail.com>
2026-03-05 15:50:35 +0600

    some validation added on ws

--- src/main/java/com/backend/messenger/ws/ChatController.java
+++ src/main/java/com/backend/messenger/ws/ChatController.java
@@ -1,6 +1,7 @@
 package com.backend.messenger.ws;
 
 import com.backend.messenger.model.Message;
+import com.backend.messenger.repository.UserRepository;
 import com.backend.messenger.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -8,6 +9,8 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.util.Map;
 
 @Controller
 public class ChatController {
@@ -18,12 +21,20 @@ public class ChatController {
     @Autowired
     private MessageService messageService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @MessageMapping("/chat.send")
     public void sendMessage(@Payload Message message) {
+        if (userRepository.findByUsername(message.getRecipient()).isEmpty()) {
+            template.convertAndSendToUser(message.getSender(), "/queue/errors",
+                    Map.of("error", "User '" + message.getRecipient() + "' does not exist"));
+            return;
+        }
+
         messageService.save(message);
         template.convertAndSendToUser(message.getRecipient(), "/queue/messages", message);
         template.convertAndSendToUser(message.getSender(), "/queue/messages", message);
-        template.convertAndSend("/topic/messages", message);
     }
 
     @MessageMapping("/chat.join")
---
+++ src/main/java/com/backend/messenger/ws/UserInterceptor.java
@@ -0,0 +1,25 @@
+package com.backend.messenger.ws;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+
+public class UserInterceptor implements ChannelInterceptor {
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+        if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
+            String username = accessor.getFirstNativeHeader("username");
+            if (username != null && !username.isBlank()) {
+                accessor.setUser(new StompPrincipal(username));
+            }
+        }
+
+        return message;
+    }
+}
